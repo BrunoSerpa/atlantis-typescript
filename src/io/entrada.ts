e6f758da326f98a6675393db9b37ffd50b072939
BrunoSerpa <serpabruno2005@gmail.com>
2024-03-08 07:24:26 -0300

    Arrumei a entrada

--- src/io/entrada.ts
+++ src/io/entrada.ts
@@ -1,24 +1,54 @@
 import promptSync from "prompt-sync";
+
 export default class Entrada {
+    private prompt = promptSync();
     public receberNumero(mensagem: string): number {
-        let prompt = promptSync();
-        let valor = prompt(`${mensagem}: `)
-        let numero = new Number(valor)
-        return numero.valueOf()
+        while (true){
+            let valor = this.prompt(mensagem)
+            let numero = parseFloat(valor)
+            if (isNaN(numero)) {
+                console.log('Insira um número!');
+            } else {
+                return numero;
+            }
+        }
     }
     public receberTexto(mensagem: string): string {
-        let prompt = promptSync();
-        let texto = prompt(`${mensagem}: `)
-        return texto
+        while (true){
+            let texto = this.prompt(mensagem)
+            if (!texto){
+                console.log("Insira algo!")
+                continue
+            }
+            return texto
+            }
     }
     public receberData(mensagem: string): Date {
-        let prompt = promptSync();
-        let texto = prompt(`${mensagem}, no padrão dd/MM/yyyy: `)
-        let partes = texto.split('/')
-        let ano = new Number(partes[2])
-        let mes = new Number(partes[1])
-        let dia = new Number(partes[0])
-        let data = new Date(ano.valueOf(), mes.valueOf() - 1, dia.valueOf())
-        return data
+        let dataEmissao: Date;
+        while (true) {
+            const data = this.prompt(mensagem);
+            const partesData = data.split('/');
+            if (partesData.length !== 3) {
+                console.log('Formato de data inválido. Use dd/mm/yyyy.');
+                continue;
+            }
+            const dia = parseInt(partesData[0], 10);
+            const mes = parseInt(partesData[1], 10);
+            const ano = parseInt(partesData[2], 10);
+            if (isNaN(dia) || isNaN(mes) || isNaN(ano)) {
+                console.log('Data inválida. Certifique-se de usar números para dia, mês e ano.');
+                continue;
+            }
+            dataEmissao = new Date(ano, mes - 1, dia);
+            if (!this.dataValida(dataEmissao)) {
+                console.log('Data de emissão inválida. Verifique a data inserida.');
+                continue;
+            }
+            break;
+        }
+        return dataEmissao;
+    }
+    private dataValida(data: Date): boolean {
+        return !isNaN(data.getTime());
     }
 }
