1d825a39a0dffff0403949d3adc7a26457f2c41f
BrunoSerpa <serpabruno2005@gmail.com>
2024-03-09 13:39:23 -0300

    Criei um menu e um tipo para o Editar e Excluir Cliente

---
+++ src/processos/tipoEditarCliente.ts
@@ -0,0 +1,32 @@
+import Processo from "../abstracoes/processo";
+import MenuTipoEditarCliente from "../menus/menuTipoEditarCliente";
+import Principal from "./principal";
+
+export default class TipoEditarCliente extends Processo {
+    constructor() {
+        super()
+        this.menu = new MenuTipoEditarCliente()
+    }
+    processar(): void {
+        this.menu.mostrar()
+        this.opcao = this.entrada.receberNumero('Qual opção desejada?')
+        
+        switch (this.opcao) {
+            case 1:
+                //this.processo = new EditarClienteTitular()
+                this.processo.processar()
+                break
+            case 2:
+                //this.processo = new EditarClienteDependente()
+                this.processo.processar()
+                break
+            case 0:
+                console.clear()
+                this.processo = new Principal()
+                this.processo.processar()
+                break
+            default:
+                console.log('Opção não entendida :(')
+        }
+    }
+}
---
+++ src/processos/tipoExcluirCliente.ts
@@ -0,0 +1,33 @@
+import Processo from "../abstracoes/processo";
+import MenuTipoExcluirCliente from "../menus/menuTipoExcluirCliente";
+import ExcluirClienteTitular from "./cadastroClienteTitular";
+import Principal from "./principal";
+
+export default class TipoExcluirCliente extends Processo {
+    constructor() {
+        super()
+        this.menu = new MenuTipoExcluirCliente()
+    }
+    processar(): void {
+        this.menu.mostrar()
+        this.opcao = this.entrada.receberNumero('Qual opção desejada?')
+        
+        switch (this.opcao) {
+            case 1:
+                this.processo = new ExcluirClienteTitular()
+                this.processo.processar()
+                break
+            case 2:
+                //this.processo = new ExcluirClienteDependente()
+                this.processo.processar()
+                break
+            case 0:
+                console.clear()
+                this.processo = new Principal()
+                this.processo.processar()
+                break
+            default:
+                console.log('Opção não entendida :(')
+        }
+    }
+}
