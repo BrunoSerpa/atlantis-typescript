801489232256a202723ef33a5258f27494d5e741
BrunoSerpa <serpabruno2005@gmail.com>
2024-03-09 22:51:01 -0300

    Adicionei a função de clonar

--- src/modelos/cliente.ts
+++ src/modelos/cliente.ts
@@ -1,10 +1,11 @@
+import Prototipo from "../interfaces/prototipo"
 import Documento from "./documento"
 import Endereco from "./endereco"
 import Telefone from "./telefone"
 
-export default class Cliente {
+export default class Cliente implements Prototipo{
     private nome: string
-    private nomeSocial: string
+    private nomeSocial!: string
     private dataNascimento: Date
     private dataCadastro: Date
     private telefones: Telefone[] = []
@@ -13,7 +14,7 @@ export default class Cliente {
     private dependentes: Cliente[] = []
     private titular!: Cliente
 
-    constructor(nome: string, nomeSocial: string, dataNascimento: Date) {
+    constructor(nome: string, nomeSocial: string | undefined, dataNascimento: Date) {
         this.nome = nome
         this.nomeSocial = nomeSocial
         this.dataNascimento = dataNascimento
@@ -31,4 +32,14 @@ export default class Cliente {
     public get Titular() { return this.titular }
 
     public set Endereco(endereco: Endereco) { this.endereco = endereco }
+    public set Titular(cliente: Cliente)  {this.titular = cliente}
+    clonar(): Prototipo {
+        let cliente = new Cliente(this.nome, this.nomeSocial, this.dataNascimento)
+        cliente.dataCadastro = this.dataCadastro
+        cliente.telefones = this.telefones
+        cliente.endereco = this.endereco
+        cliente.documentos = this.documentos
+        cliente.dependentes = this.dependentes
+        return cliente
+    }
 }
