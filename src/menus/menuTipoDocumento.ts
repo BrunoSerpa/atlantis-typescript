2140ea180a5de0919faed15c7c3ea921310e1ab5
BrunoSerpa <serpabruno2005@gmail.com>
2024-03-10 00:03:27 -0300

    Acrescentei no cadastro de documentos o Cpf e o Passaporte

--- src/menus/menuTipoDocumento.ts
+++ src/menus/menuTipoDocumento.ts
@@ -9,7 +9,7 @@ export default class MenuTipoDocumento implements Menu {
         console.log(`| 1 - Cadastro de Pessoas Física`)
         console.log(`| 2 - Registro Geral`)
         console.log(`| 3 - Passaporte`)
-        console.log(`| 0 - Finalizar cadastro de documentos`)
+        console.log(`| 0 - Cancelar`)
         console.log(`----------------------`)
     }
 }
