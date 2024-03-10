76a8b29fe0aa725666ce10065733261324d7650a
BrunoSerpa <serpabruno2005@gmail.com>
2024-03-10 00:09:42 -0300

    Acrescentei os impressores de dependentes e de telefones; Arrumei bugs de espaços vazios

--- src/impressores/impressorCliente.ts
+++ src/impressores/impressorCliente.ts
@@ -1,7 +1,9 @@
 import Impressor from "../interfaces/impressor";
 import Cliente from "../modelos/cliente";
+import ImpressaorDependentes from "./impressorDependentes";
 import ImpressorDocumentos from "./impressorDocumentos";
 import ImpressorEndereco from "./impressorEndereco";
+import ImpressorTelefones from "./impressorTelefones";
 
 export default class ImpressaorCliente implements Impressor {
     private cliente: Cliente
@@ -14,15 +16,27 @@ export default class ImpressaorCliente implements Impressor {
     imprimir(): string {
         let impressao = `****************************\n`
             + `| Nome: ${this.cliente.Nome}\n`
-            + `| Nome social: ${this.cliente.NomeSocial}\n`
+            + ((this.cliente.NomeSocial) ? `| Nome social: ${this.cliente.NomeSocial}\n` : ``)
             + `| Data de nascimento: ${this.cliente.DataNascimento.toLocaleDateString()}\n`
             + `| Data de cadastro: ${this.cliente.DataCadastro.toLocaleDateString()}`
+
+        if (this.cliente.Telefones.length >= 1){
+            this.impressor = new ImpressorTelefones(this.cliente.Telefones)
+            impressao = impressao + `\n${this.impressor.imprimir()}`
+        }
 
         this.impressor = new ImpressorEndereco(this.cliente.Endereco)
         impressao = impressao + `\n${this.impressor.imprimir()}`
 
-        this.impressor = new ImpressorDocumentos(this.cliente.Documentos)
-        impressao = impressao + `\n${this.impressor.imprimir()}`
+        if (this.cliente.Documentos.length >= 1){
+            this.impressor = new ImpressorDocumentos(this.cliente.Documentos)
+            impressao = impressao + `\n${this.impressor.imprimir()}`
+        }
+
+        if (this.cliente.Dependentes.length >= 1){
+            this.impressor = new ImpressaorDependentes(this.cliente.Dependentes)
+            impressao = impressao + `\n${this.impressor.imprimir()}`
+        }
 
         impressao = impressao + `\n****************************`
         return impressao
